8352e88e0256162fd154fb0caa2fb2b84bb5a62c
Vidyasagar Ananthan <vidyaaws@amazon.com>
2023-08-02 12:24:39 -0700

    GH-11: Removing redundant command on stop.

--- examples/utils/stop_and_delete.py
+++ examples/utils/stop_and_delete.py
@@ -1,9 +1,9 @@
 import os
 
-try:
-    os.system('redis-cli flushall')    
-except:
-    pass
+# try:
+#     os.system('redis-cli flushall')    
+# except:
+#     pass
 
 try:
     os.system("pkill -9 -f 'celerytasks'")     
